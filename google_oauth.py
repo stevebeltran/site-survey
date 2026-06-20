ff14e106807e8c73cf5ee2174add723596e2a16e
stevebeltran <steven.beltran@brincdrones.com>
2026-06-20 11:42:24 -0500

    fix: use full googleapis email scope, gitignore client secrets

--- google_oauth.py
+++ google_oauth.py
@@ -14,7 +14,7 @@ import streamlit as st
 
 SCOPES = [
     "openid",
-    "email",
+    "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/gmail.readonly",
     "https://www.googleapis.com/auth/calendar.readonly",
     "https://www.googleapis.com/auth/drive",
